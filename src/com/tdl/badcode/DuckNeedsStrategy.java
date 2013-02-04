3be29e07d163d02f23901034d534452d0bea3a74
Aleksey Drobnych <adrobnych@gmail.com>
2013-02-04 13:27:03 +0200

    modified bad sample for strategy

--- src/com/tdl/badcode/DuckNeedsStrategy.java
+++ src/com/tdl/badcode/DuckNeedsStrategy.java
@@ -1,11 +1,16 @@
 package com.tdl.badcode;
 
-class Duck{
+abstract class Duck{                                        // we need this base class for polymorphic actions
+	
 	public void quack(){
-		System.out.println("Quack");
+		System.out.println("Default Quack");                // we need polymorphic calls to quack()
 	}
 	public void fly(){
-		System.out.println("fly");
+		System.out.println("Default Fly");                  // we need polymorphic calls to fly()
+	}
+	
+	public void getLocation(){
+		System.out.println("My location is beautiful lake");
 	}
 }
 
@@ -23,12 +28,34 @@ class RibbonDuck extends Duck{
 	public void quack(){
 		System.out.println("Squick");
 	}	
+	
+	// no fly ability! notice this!
+}
+
+class DivingDuck extends Duck{
+	public void quack(){
+		System.out.println("Quack Quack");               // Hmmm. The same as for MallardDuck! don't DRY!
+	}
+	public void fly(){
+		System.out.println("fly..............");
+	}	
+}
+
+class GoldenEyeDuck extends Duck{
+	public void quack(){
+		System.out.println("Quack Quack");               // Hmmm. The same as for MallardDuck! don't DRY!
+	}
+	public void fly(){
+		System.out.println("fly..............");
+	}	
 }
 
 public class DuckNeedsStrategy{
 	public static void main(String[] args){
-		Duck rd = new RibbonDuck();
-		rd.fly();
+		Duck d = new RibbonDuck();
+		d.fly();
 		// but ribbon duck can't fly!
+		d = new GoldenEyeDuck();
+		d.quack();
 	}
 }
